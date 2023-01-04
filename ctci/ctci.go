3148bcb2f2260349151b35cdaaaace0aff7d41e3
Anthony <anthonymagallanes24@gmail.com>
2023-01-03 22:51:33 -0800

    feat(checkPerm): checkPermutation

--- ctci/ctci.go
+++ ctci/ctci.go
@@ -18,3 +18,47 @@ func IsUnique(str string) bool {
 	// return true
 	return true
 }
+
+func CheckPermutation(str1 string, str2 string) bool {
+
+	// check if length of str1 is not equal to lengh of str2
+		// return false
+	if (len(str1) != len(str2)) {
+		return false
+	}
+
+	// create an empty map
+	charMap := make(map[string]int)
+
+	// iterate through str1
+		// check if obj has char
+			// increment obj char value by 1
+		// else
+			// add char to obj with value of 1
+	for _, char := range str1 {
+		if charMap[string(char)] >= 1 {
+			charMap[string(char)]++
+		} else {
+			charMap[string(char)] = 1
+		}
+	}
+
+	// iterate through str2
+		// check if char is in obj
+			// decrement obj char value by 1
+			// check if obj char value is equal to 0
+				// delete map char
+	for _, char := range str2 {
+		if charMap[string(char)] >= 1 {
+			charMap[string(char)]--
+			if (charMap[string(char)] == 0) {
+				delete(charMap, string(char))
+			}
+		}
+	}
+	// check if map is not empty
+		// return false
+	// else
+		// return true
+	return len(charMap) <= 0
+}
