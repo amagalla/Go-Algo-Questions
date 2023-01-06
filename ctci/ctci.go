20c7078396a4a51a6fbc0d1f6cfda72d327db2d3
Anthony <anthonymagallanes24@gmail.com>
2023-01-05 20:53:31 -0800

    feat(algos): refactored

--- ctci/ctci.go
+++ ctci/ctci.go
@@ -6,6 +6,8 @@ import (
 	"unicode/utf8"
 )
 
+// Implement an algorithm to determine if a string has all unique characters.
+
 func IsUnique(str string) bool {
 	// check is str is empty
 	if len(str) <= 0 {
@@ -29,6 +31,9 @@ func IsUnique(str string) bool {
 	// return true
 	return true
 }
+
+// Check Permutation: Given two strings, write a method to decide if one is
+// as permutation of the other.
 
 func CheckPermutation(str1 string, str2 string) bool {
 
@@ -73,6 +78,23 @@ func CheckPermutation(str1 string, str2 string) bool {
 		// return true
 	return len(charMap) <= 0
 }
+
+/*
+
+	Given a string, write a function to check if it is a permutation
+	of a palindrome. A palindrome is a word or phrase that is the
+	same forward and backwards. A permutation is a rearrangement
+	of letters. The palindrome does not need to be limited to just
+	dictionary words.
+	You can ignore casing and non-letter characters.
+
+	EXAMPLE
+
+	Input: Tact Coa
+
+	Output: True (permutation: "taco cat", "atco cta", etc).
+
+*/
 
 func PalindromePermutation(str string) bool {
 	// check if string is empty
@@ -130,6 +152,20 @@ func PalindromePermutation(str string) bool {
 	// return isOdd
 	return isOdd
 }
+
+/*
+	There are three types of edits that can be performed on strings: insert a character,
+	remove a character, or replace a character. Given two strings, write a function to
+	check if they are one edit (or zero edits) away.
+
+	EXAMPLE
+
+	pale, ple -> true
+	pales, pale -> true
+	pale, bale -> true
+	pale, bake -> false
+
+*/
 
 func OneWay(str1 string, str2 string) bool {
 	// check if str2 length is larger than str1 length + 1
@@ -192,6 +228,16 @@ func OneWay(str1 string, str2 string) bool {
 
 	return true
 }
+
+/*
+
+	Implement a method to peform basic string compression using the counts of
+	repeated characters. For example, the string aabcccccaaa would become
+	a2b1c5a3. If the "compressed" string would not become smaller than the
+	original string, your method should return the original string. You can assume
+	the string has only uppercase and lowercase letters (a-z).
+
+*/
 
 func StringCompression(str string) string {
 	// create a new empty str
--- main.go
+++ main.go
@@ -7,23 +7,17 @@ import (
 )
 
 func main() {
-
 	
-	// Implement an algorithm to determine if a string has all unique characters.
-
 	fmt.Println("IsUnique")
 	fmt.Println("")
 
 	fmt.Println(1, ctci.IsUnique("abcd")) // true
-	fmt.Println(2, ctci.IsUnique("abcdabcd")) // false
+	fmt.Println(2, ctci.IsUnique("abcdabcd")) //  false
 	fmt.Println(3, ctci.IsUnique("Hanzi")) // true
 	fmt.Println(4, ctci.IsUnique("")) // false
 
 	fmt.Println("______________________")
 	fmt.Println("")
-
-	// Check Permutation: Given two strings, write a method to decide if one is
-	// as permutation of the other.
 
 	fmt.Println("CheckPermutation")
 	fmt.Println("")
@@ -39,22 +33,6 @@ func main() {
 	fmt.Println("______________________")
 	fmt.Println("")
 
-	/*
-
-	Given a string, write a function to check if it is a permutation
-	of a palindrome. A palindrome is a word or phrase that is the
-	same forward and backwards. A permutation is a rearrangement
-	of letters. The palindrome does not need to be limited to just
-	dictionary words.
-	You can ignore casing and non-letter characters.
-
-	EXAMPLE
-
-	Input: Tact Coa
-
-	Output: True (permutation: "taco cat", "atco cta", etc).
-
-	*/
 
 	fmt.Println("PalindromePermutation")
 	fmt.Println("")
@@ -70,19 +48,6 @@ func main() {
 	fmt.Println("______________________")
 	fmt.Println("")
 
-	/*
-	There are three types of edits that can be performed on strings: insert a character,
-	remove a character, or replace a character. Given two strings, write a function to
-	check if they are one edit (or zero edits) away.
-
-	EXAMPLE
-
-	pale, ple -> true
-	pales, pale -> true
-	pale, bale -> true
-	pale, bake -> false
-
-	*/
 
 	fmt.Println("OneWay")
 	fmt.Println("")
@@ -101,17 +66,10 @@ func main() {
 
 	fmt.Println("______________________")
 	fmt.Println("")
-	
-	
-	/*
 
-	Implement a method to peform basic string compression using the counts of
-	repeated characters. For example, the string aabcccccaaa would become
-	a2b1c5a3. If the "compressed" string would not become smaller than the
-	original string, your method should return the original string. You can assume
-	the string has only uppercase and lowercase letters (a-z).
 
-	*/
+	fmt.Println("StringCompression")
+	fmt.Println("")
 
 	fmt.Println(1, ctci.StringCompression("aabcccccaaa")) // a2b1c5a3
 	fmt.Println(1, ctci.StringCompression("aabccccc")) // a2b1c5
